7dabc494af7e6aa1bb6081df41b6b2bbb2a3d897
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-01-28 01:08:42 +0900

    コントローラテストの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
+    nickname              {"testname"}
     password              {"00000000"}
     password_confirmation {"00000000"}
+    sequence(:email) {Faker::Internet.email}
   end
 
 end
